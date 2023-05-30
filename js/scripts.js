e1427a8773f5bdae36543a00242b92b21a72dd6b
BeJames <tryfindingjames@gmail.com>
2023-05-30 18:44:01 -0500

    picking up projet for new work

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,11 @@ function divide(number1, number2) {
 }
 
 // user interface logic 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(
-number1 + " + " + number2 + " = " + add(number1, number2) +". " +
-number1 + " - " + number2 + " = " + subtract(number1, number2) +". " +
-number1 + " * " + number2 + " = " + multiply(number1, number2) +". " +
-number1 + " / " + number2 + " = " + divide(number1, number2));
+// window.alert(
+// number1 + " + " + number2 + " = " + add(number1, number2) +". " +
+// number1 + " - " + number2 + " = " + subtract(number1, number2) +". " +
+// number1 + " * " + number2 + " = " + multiply(number1, number2) +". " +
+// number1 + " / " + number2 + " = " + divide(number1, number2));
